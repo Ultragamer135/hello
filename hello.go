f8af45f830da2442f75a71ff49040a315f27948a
usernamenotfound <elinero27@gmail.com>
2020-11-04 18:11:07 -0500

    hi

--- hello.go
+++ hello.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 
 	"github.com/UsernameNotAvalible/hello/morestrings"
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
